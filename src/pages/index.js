f6123667c1364cb9e9a157ba7c14f056884cc28d
psriprac <psriprac@gmail.com>
2022-02-16 00:41:16 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,6 @@ import * as React from "react"
 const IndexPage = () => {
   return (
     <div>
-      <head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </head>
       Hello World
     </div>  
   )
